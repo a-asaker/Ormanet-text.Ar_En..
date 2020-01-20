bd8d0032c78a0fbd9f1e8887c66fd6014a94e5b2
A_Asaker <39923238+a-asaker@users.noreply.github.com>
2020-01-20 22:09:49 +0100

    Update and rename P2.java to ornament/Main.java

---
+++ ornament/Main.java
@@ -0,0 +1,25 @@
+package ornament;
+
+import com.sun.webkit.graphics.GraphicsDecoder;
+import java.awt.Image;
+import java.io.File;
+import java.text.ParseException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.ImageIcon;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+/**
+ *
+ * @author a-asaker
+ */
+public class Main{
+    public static void main(String[] args) {
+            Ornament form = new Ornament(); 
+           form.setVisible(true);
+           ImageIcon img = new ImageIcon("iaco.jpg");
+         form.setIconImage(img.getImage());
+       }
+    
+}
